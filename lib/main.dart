b8d7c7721001d556780c31e4679f5ffb14aab2aa
lera-github <leragithub@gmail.com>
2023-05-18 14:37:01 +0300

    0

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home:  MyHomePage(collectionPath: '', usermapdata: {},),
+      home:  const MyHomePage(collectionPath: '', usermapdata: {},),
     );
   }
 }
